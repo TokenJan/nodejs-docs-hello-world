d6efbdd30a0de2e76aa69f55b94e883ae3a30df6
Jiahuan Yang <jiahuan.yang@outlook.com>
2019-08-16 11:18:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! This is Nodejs.");
+    response.end("Hello World! This is staging.");
 });
 
-const port = 1338;
+const port = process.env.PORT || 1338;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
